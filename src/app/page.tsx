c75cac3a0de3ffb64285cb9800d36280d0ee27b6
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-04-30 10:38:30 +0530

    phase 0

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>FlowMate</h1>
+      <p>No-Code Automation Platform (Prototype)</p>
+    </div>
+  );
+}
